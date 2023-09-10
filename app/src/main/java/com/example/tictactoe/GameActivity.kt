9a32497c5d39c416646a2165099138040ca5ec3f
LordGum <dari.vision@mail.ru>
2023-09-10 20:50:35 +0300

    commit 4: add logic in MainActivity

--- app/src/main/java/com/example/tictactoe/GameActivity.kt
+++ app/src/main/java/com/example/tictactoe/GameActivity.kt
@@ -1,5 +1,7 @@
 package com.example.tictactoe
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +9,17 @@ class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+    }
+
+    companion object {
+        fun newIntent(context: Context, time: Long, gameField: String): Intent {
+            val intent = Intent(context, GameActivity::class.java)
+            intent.putExtra(EXTRA_TIME, time)
+            intent.putExtra(EXTRA_GAME_FIELD, gameField)
+            return intent
+        }
+
+        private const val EXTRA_TIME = "Time"
+        private const val EXTRA_GAME_FIELD = "GameField"
     }
 }
--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -2,22 +2,49 @@
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tictactoe.databinding.ActivityMainBinding
 
    class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+       private lateinit var binding: ActivityMainBinding
+       override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        /*
-        binding = ResultProfileBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
 
-         */
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.startButtonMain.setOnClickListener {
+            val data = getInfoGame()
+            val intent = GameActivity.newIntent(this@MainActivity,data.time, data.gameField)
+            startActivity(intent)
+        }
+        binding.settingsMain.setOnClickListener {
+            val data = getInfoGame()
+            val intent = SettingActivity.newIntent(this@MainActivity, data.time, data.gameField)
+            startActivity(intent)
+        }
+        binding.continueGameMain.setOnClickListener {
+            val data = getInfoGame()
+            val intent = GameActivity.newIntent(this@MainActivity, data.time, data.gameField)
+            startActivity(intent)
+        }
+        setContentView(binding.root)
+       }
+
+       data class InfoGame(val time: Long, val gameField: String){}
+
+       private fun getInfoGame(): InfoGame {
+           with(getSharedPreferences(getString(R.string.preference_file_key), MODE_PRIVATE)) {
+               val time = getLong("time", 0L)
+               val gameField = getString("gameField", "")
+
+               return if(gameField != null) {
+                   InfoGame(time, gameField)
+               } else {
+                   InfoGame(0L, "")
+               }
+           }
+       }
 
 
-
-        val intent = SettingActivity.newIntent(this@MainActivity)
-        startActivity(intent)
-    }
 }
 
