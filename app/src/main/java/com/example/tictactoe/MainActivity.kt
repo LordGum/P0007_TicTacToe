850900cfabd1036489e8360cb8f34b26621922aa
LordGum <dari.vision@mail.ru>
2023-09-11 19:06:13 +0300

    commit 5: almost done

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -1,50 +1,57 @@
-   package com.example.tictactoe
+package com.example.tictactoe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.tictactoe.databinding.ActivityMainBinding
 
-   class MainActivity : AppCompatActivity() {
-       private lateinit var binding: ActivityMainBinding
-       override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setTheme(R.style.Theme_TicTacToe)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         binding.startButtonMain.setOnClickListener {
-            val data = getInfoGame()
-            val intent = GameActivity.newIntent(this@MainActivity,data.time, data.gameField)
+            val intent = Intent(this@MainActivity, GameActivity::class.java)
             startActivity(intent)
         }
         binding.settingsMain.setOnClickListener {
-            val data = getInfoGame()
-            val intent = SettingActivity.newIntent(this@MainActivity, data.time, data.gameField)
+            val intent = Intent(this@MainActivity, SettingsActivity::class.java)
             startActivity(intent)
         }
         binding.continueGameMain.setOnClickListener {
-            val data = getInfoGame()
-            val intent = GameActivity.newIntent(this@MainActivity, data.time, data.gameField)
+            val gameInfo = getInfoAboutLastGame()
+            val intent = Intent(this@MainActivity, GameActivity::class.java).apply {
+                putExtra(EXTRA_TIME, gameInfo.time)
+                putExtra(EXTRA_GAME_FIELD, gameInfo.gameField)
+            }
             startActivity(intent)
         }
         setContentView(binding.root)
        }
 
-       data class InfoGame(val time: Long, val gameField: String){}
+    private fun getInfoAboutLastGame() : GameInfo {
+        with(getSharedPreferences("game", MODE_PRIVATE)){
+            val time = getLong("time", 0)
+            val gameField = getString("gameField", "")
 
-       private fun getInfoGame(): InfoGame {
-           with(getSharedPreferences(getString(R.string.preference_file_key), MODE_PRIVATE)) {
-               val time = getLong("time", 0L)
-               val gameField = getString("gameField", "")
+            return if (gameField != null) {
+                GameInfo(time, gameField)
+            } else {
+                GameInfo(0,"")
+            }
+        }
+    }
 
-               return if(gameField != null) {
-                   InfoGame(time, gameField)
-               } else {
-                   InfoGame(0L, "")
-               }
-           }
-       }
+    data class GameInfo(val time: Long, val gameField: String)
 
+    companion object {
+        const val EXTRA_TIME = "my.tick_tac_toe.TIME"
+        const val EXTRA_GAME_FIELD = "my.tick_tac_toe.GAME_FIELD"
+    }
 
 }
 
---
+++ app/src/main/java/com/example/tictactoe/SettingsActivity.kt
@@ -0,0 +1,179 @@
+package com.example.tictactoe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.SeekBar
+import com.example.tictactoe.databinding.ActivitySettingBinding
+
+class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var settingsBinding: ActivitySettingBinding
+
+    private var currentLevel : Int = 0
+    private var currentVolumeSound: Int = 0
+    private var currentRules: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        settingsBinding = ActivitySettingBinding.inflate(layoutInflater)
+
+        val currentSettings = getCurrentSettings()
+
+        currentLevel = currentSettings.level
+        currentVolumeSound = currentSettings.sound
+        currentRules = currentSettings.rules
+
+        if(currentLevel == 0){
+            settingsBinding.previousSet.visibility = View.INVISIBLE
+        } else if (currentLevel == 2) {
+            settingsBinding.nextSet.visibility = View.INVISIBLE
+        }
+
+        settingsBinding.levelInGameSet.text = resources.getStringArray(R.array.game_level)[currentLevel]
+        settingsBinding.seekBar.progress = currentVolumeSound
+
+        when(currentSettings.rules){
+            1 -> settingsBinding.checkBoxHorizontal.isChecked = true
+            2 -> settingsBinding.checkBoxVertical.isChecked = true
+            3 -> {
+                settingsBinding.checkBoxHorizontal.isChecked = true
+                settingsBinding.checkBoxVertical.isChecked = true
+            }
+            4 -> settingsBinding.checkBoxDiagonal.isChecked = true
+            5 -> {
+                settingsBinding.checkBoxDiagonal.isChecked = true
+                settingsBinding.checkBoxHorizontal.isChecked = true
+            }
+            6 -> {
+                settingsBinding.checkBoxDiagonal.isChecked = true
+                settingsBinding.checkBoxVertical.isChecked = true
+            }
+            7 -> {
+                settingsBinding.checkBoxHorizontal.isChecked = true
+                settingsBinding.checkBoxVertical.isChecked = true
+                settingsBinding.checkBoxDiagonal.isChecked = true
+            }
+        }
+
+        settingsBinding.previousSet.setOnClickListener {
+            currentLevel--
+
+            if(currentLevel == 0){
+                settingsBinding.previousSet.visibility = View.INVISIBLE
+            } else if (currentLevel == 1) {
+                settingsBinding.nextSet.visibility = View.VISIBLE
+            }
+
+            updateLevel(currentLevel)
+            settingsBinding.levelInGameSet.text = resources.getStringArray(R.array.game_level)[currentLevel]
+        }
+
+        settingsBinding.nextSet.setOnClickListener {
+            currentLevel++
+
+            if(currentLevel == 2){
+                settingsBinding.nextSet.visibility = View.INVISIBLE
+            } else if (currentLevel == 1) {
+                settingsBinding.previousSet.visibility = View.VISIBLE
+            }
+
+            updateLevel(currentLevel)
+            settingsBinding.levelInGameSet.text = resources.getStringArray(R.array.game_level)[currentLevel]
+        }
+
+        settingsBinding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, progress: Int, p2: Boolean) {
+                currentVolumeSound = progress
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+                updateVolumeSound(currentVolumeSound)
+            }
+
+        })
+
+        settingsBinding.checkBoxHorizontal.setOnCheckedChangeListener { _, isChecked ->
+            if(isChecked){
+                currentRules += 1
+            } else {
+                currentRules -= 1
+            }
+
+            updateRules(currentRules)
+        }
+
+        settingsBinding.checkBoxVertical.setOnCheckedChangeListener { _, isChecked ->
+            if(isChecked){
+                currentRules += 2
+            } else {
+                currentRules -= 2
+            }
+
+            updateRules(currentRules)
+        }
+
+        settingsBinding.checkBoxDiagonal.setOnCheckedChangeListener { _, isChecked ->
+            if(isChecked){
+                currentRules += 4
+            } else {
+                currentRules -= 4
+            }
+
+            updateRules(currentRules)
+        }
+
+        settingsBinding.backButtonSet.setOnClickListener {
+            setResult(RESULT_OK)
+            onBackPressed()
+        }
+
+        setContentView(settingsBinding.root)
+    }
+
+    private fun updateVolumeSound(volume: Int){
+        getSharedPreferences("game", MODE_PRIVATE).edit().apply{
+            putInt(PREF_SOUND, volume)
+            apply()
+        }
+        setResult(RESULT_OK)
+    }
+
+    private fun updateLevel(level: Int){
+        getSharedPreferences("game", MODE_PRIVATE).edit().apply {
+            putInt(PREF_LEVEL, level)
+            apply()
+        }
+        setResult(RESULT_OK)
+    }
+
+    private fun updateRules(rules: Int){
+        getSharedPreferences("game", MODE_PRIVATE).edit().apply {
+            putInt(PREF_RULES, rules)
+            apply()
+        }
+        setResult(RESULT_OK)
+    }
+
+    private fun getCurrentSettings(): SettingInfo {
+        this.getSharedPreferences("game", MODE_PRIVATE).apply {
+
+            val sound = getInt(PREF_SOUND, 100)
+            val level = getInt(PREF_LEVEL, 1)
+            val rules = getInt(PREF_RULES, 7)
+
+            return SettingInfo(sound, level, rules)
+        }
+    }
+
+    data class SettingInfo(val sound: Int, val level: Int, val rules: Int)
+
+    companion object {
+        const val PREF_SOUND = "my.tick_tac_toe.SOUND"
+        const val PREF_LEVEL = "my.tick_tac_toe.LEVEL"
+        const val PREF_RULES = "my.tick_tac_toe.RULES"
+    }
+}
